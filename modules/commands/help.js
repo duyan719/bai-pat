943007aac380a0eecf1bd718cc199399c3571c95
duyan719 <duyan719@gmail.com>
2021-09-26 00:58:24 +0700

    first commit

--- modules/commands/help.js
+++ modules/commands/help.js
@@ -44,9 +44,8 @@ module.exports.run = function({ api, event, args }) {
 			if (!group.some(item => item.group.toLowerCase() == commandConfig.config.commandCategory.toLowerCase())) group.push({ group: commandConfig.config.commandCategory.toLowerCase(), cmds: [commandConfig.config.name] });
 			else group.find(item => item.group.toLowerCase() == commandConfig.config.commandCategory.toLowerCase()).cmds.push(commandConfig.config.name);
 		}
-		group.forEach(commandGroup => msg += `『 ${commandGroup.group.charAt(0).toUpperCase() + commandGroup.group.slice(1)} 』\n${commandGroup.cmds.join(', ')}\n\n`);
-		return api.sendMessage(msg + `[ Sử dụng: "${(threadSetting.hasOwnProperty("PREFIX")) ? threadSetting.PREFIX : global.config.PREFIX}help từng lệnh ở trên" để xem chi tiết cách sử dụng! 
-    -Hiện tại đang có ${commands.size} lệnh có thể sử dụng trên bot này.
+		return api.sendMessage(msg + `「Trợ Giúp」\n menu\n\n[ Sử dụng: "${(threadSetting.hasOwnProperty("PREFIX")) ? threadSetting.PREFIX : global.config.PREFIX}help từng lệnh ở trên" để xem chi tiết cách sử dụng! 
+    -Hiện tại đang có 1 lệnh có thể sử dụng trên bot này.
 👉 Liên hệ admin Bot để biết thêm thông tin về bot | https://www.facebook.com/nguyenduyan29072004/ |
 -❤YÊU 3000❤-
 -Dùng lệnh /callad để báo lỗi Bot cho admin nhé!!! ]
